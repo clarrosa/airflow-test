8fc893019bf2300bc9369628753eff3d12b29d20
clarrosa <53796838+clarrosa@users.noreply.github.com>
2021-12-01 20:59:34 -0300

    Update slack.py

--- slack.py
+++ slack.py
@@ -3,7 +3,6 @@ from slack_sdk import WebClient
 
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 CHANNEL_ID = os.environ["CHANNEL_ID"]
-print(SLACK_BOT_TOKEN)
 
 client = WebClient(token=SLACK_BOT_TOKEN)
 channel_id = CHANNEL_ID
